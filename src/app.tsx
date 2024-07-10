82e29217053f2aed652cabb22f16f148f8e243d3
lais <laisnicmns@gmail.com>
2024-07-10 17:36:51 -0300

    iniciando tudo

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+
+export function App() { //export antes de function + chamando daquela forma no main evita erros de importação por nome incorreto
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", //arquivos da minha estilização que possam conter tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
